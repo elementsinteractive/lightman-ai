049aa8784f5bb0eb108a0f012dd359268a169ba0
sdn4z <13658011+sdn4z@users.noreply.github.com>
2025-04-24 09:26:02 +0200

    feat: run prompt multiple times and introduce pydantic settings

--- src/hackerman_ai/ai/models.py
+++ src/hackerman_ai/ai/models.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import StrEnum
+from typing import override
 
 
 class Priority(StrEnum):
@@ -11,12 +12,27 @@ class Priority(StrEnum):
 @dataclass
 class New:
     title: str
-    priority: Priority
     description: str
-    why_is_relevant: str
     link: str
+
+    @override
+    def __eq__(self, value: object) -> bool:
+        assert isinstance(value, New)
+        return self.link == value.link
+
+    @override
+    def __hash__(self) -> int:
+        return hash(self.link.encode())
 
 
 @dataclass
 class News:
     news: list[New]
+
+    @property
+    def titles(self) -> list[str]:
+        return [new.title for new in self.news]
+
+    @property
+    def links(self) -> list[str]:
+        return [new.link for new in self.news]
