1d130acc762978180b77c9585dc0a21cd2eeb907
sdn4z <13658011+sdn4z@users.noreply.github.com>
2025-05-09 11:54:37 +0200

    chore: Articles have now number_of_tokens property

--- src/hackerman_ai/core/settings.py
+++ src/hackerman_ai/core/settings.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
+    OPENAI_MODEL: str = "gpt-4o"
     PROMPT_ITERATIONS: int = 3
 
 
