4d5a3d7d5a93e42d133645cc66474c692211c6e7
sdn4z <13658011+sdn4z@users.noreply.github.com>
2025-05-19 16:42:44 +0200

    chore: Create tooling for the agent to run multiple times

--- src/hackerman_ai/cli.py
+++ src/hackerman_ai/cli.py
@@ -12,12 +12,13 @@ def entry_point() -> None:
 
 @entry_point.command()
 @click.option("--model", type=click.Choice(MODEL_CHOICES), help=("Which model to use."), default="gpt-4.1")
-def run(model: str) -> int:
+@click.option("--iterations", type=int, help=("The number of iterations that the agent will run"), default=None)
+def run(model: str, iterations: int | None) -> int:
     """
     Entrypoint of the application.
 
     Holds no logic. Just calls the methods and return an exit code.
     """
-    asyncio.run(hackerman(model))
+    asyncio.run(hackerman(model, iterations))
 
     return 0
