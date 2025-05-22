b5c15cf20a12e696b2cb628337e79d6d87f9b984
sdn4z <13658011+sdn4z@users.noreply.github.com>
2025-05-22 11:18:49 +0200

    refactor: move base agent tests to another directory

--- tests/ai/openai/test_agent.py
+++ tests/ai/openai/test_agent.py
@@ -1,7 +1,6 @@
 import pytest
 from hackerman_ai.ai.openai.agent import OpenAIAgent
-from hackerman_ai.article.models import SelectedArticle, SelectedArticlesList
-from tests.conftest import patch_run_prompt
+from hackerman_ai.article.models import SelectedArticlesList
 
 
 class TestAgent:
@@ -12,33 +11,3 @@ class TestAgent:
         agent = OpenAIAgent(self.OPENAI_MODEL)
         result = await agent._run_prompt(short_prompt)
         assert isinstance(result, SelectedArticlesList)
-
-    async def test_run_prompt_multiple_times(self, short_prompt: str) -> None:
-        """Test that we run the prompt as many times as we specify, and we receive back a LIST of `SelectedArticlesList`."""
-        agent = OpenAIAgent(self.OPENAI_MODEL)
-        async with patch_run_prompt() as mock:
-            result = await agent._run_prompt_multiple_times(short_prompt, iterations=3)
-        assert mock.call_count == 3
-        assert isinstance(result, list)
-        assert len(result) == 3
-        assert all(isinstance(x, SelectedArticlesList) for x in result)
-
-    async def test_get_prompt_result(self, short_prompt: str) -> None:
-        """Test that we run the prompt as many times as we specify, and we receive back an INSTANCE of `SelectedArticlesList`."""
-        agent = OpenAIAgent(self.OPENAI_MODEL)
-        async with patch_run_prompt() as mock:
-            result = await agent.get_prompt_result(short_prompt, iterations=3)
-        assert mock.call_count == 3
-        assert isinstance(result, SelectedArticlesList)
-
-    def test__merge_results(self) -> None:
-        article1 = SelectedArticle(link="link1")
-        article1_repeated = SelectedArticle(link="link1")
-        different_article = SelectedArticle(link="link2")
-        articles_list1 = SelectedArticlesList(articles=[article1, different_article])
-        articles_list2 = SelectedArticlesList(articles=[article1_repeated])
-
-        agent = OpenAIAgent(self.OPENAI_MODEL)
-        result = agent._merge_results([articles_list1, articles_list2])
-        assert isinstance(result, SelectedArticlesList)
-        assert set(result.articles) == {article1, different_article}
