a2dbfc8f5085fb41338a08386602e1647a561a43
sdn4z <13658011+sdn4z@users.noreply.github.com>
2025-06-19 16:23:21 +0200

    feat: allow config file in eval

--- eval/cli.py
+++ eval/cli.py
@@ -6,6 +6,7 @@ from hackerman_ai.ai.utils import MODEL_CHOICES
 from eval.classified_articles import NON_RELEVANT_ARTICLES, RELEVANT_ARTICLES
 from eval.constants import DEFAULT_MODEL
 from eval.evaluator import eval
+from eval.utils import EvalConfig, EvalFileConfig
 
 
 @click.command()
@@ -14,34 +15,35 @@ from eval.evaluator import eval
     "--model", type=click.Choice(MODEL_CHOICES), help=("The model to use to analyze articles"), default=DEFAULT_MODEL
 )
 @click.option("--iterations", type=int, help=("Number of times that the prompt will run"), default=1)
-@click.option("--score", type=int, help=("Minimum score to consider an article to be relevant"), default=8)
+@click.option("--score", type=int, help=("Minimum score to consider an article to be relevant"))
 @click.option(
     "--samples",
     type=int,
     help=(
         "Number of times the evaluation will run with all its iterations. It will calculate averages for the results. "
     ),
-    default=1,
 )
-@click.option("--prompt", type=click.Choice(PROMPTS_CHOICES), help=("Which prompt to use."), default="eval")
+@click.option("--prompt", type=click.Choice(PROMPTS_CHOICES), help=("Which prompt to use."))
 def run(model: str, iterations: int, score: int, samples: int, prompt: str, tag: str | None = None) -> None:
-    def validate_positive_int(field: str, value: int) -> None:
-        if value is not None and value < 1:
-            raise click.BadParameter(f"`{field}` must be > 0.")
-
-    validate_positive_int("samples", samples)
-    validate_positive_int("score", score)
-    validate_positive_int("iterations", iterations)
-
+    config_from_file = EvalFileConfig.get_config_from_file(config_section="eval")
+    config = EvalConfig.init_from_dict(
+        {
+            "model": model or config_from_file.model,
+            "prompt": prompt or config_from_file.prompt,
+            "score_threshold": score or config_from_file.score_threshold,
+            "iterations": iterations or config_from_file.iterations,
+            "samples": samples or config_from_file.samples,
+        }
+    )
     eval(
-        score_threshold=score,
-        iterations=iterations,
+        score_threshold=config.score_threshold,
+        iterations=config.iterations,
         relevant_articles=RELEVANT_ARTICLES,
         non_relevant_articles=NON_RELEVANT_ARTICLES,
-        samples=samples,
+        samples=config.samples,
         tag=tag,
-        model=model,
-        prompt=prompt,
+        model=config.model,
+        prompt=config.prompt,
     )
 
 
