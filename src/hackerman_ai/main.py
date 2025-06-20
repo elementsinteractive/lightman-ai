e929a1ac8d3887a60effdfd98a6296d05f462554
sdn4z <13658011+sdn4z@users.noreply.github.com>
2025-06-20 15:00:34 +0200

    feat: create prompts in toml file

--- src/hackerman_ai/main.py
+++ src/hackerman_ai/main.py
@@ -1,7 +1,6 @@
 import logging
 
 from hackerman_ai.ai.base.agent import BaseAgent
-from hackerman_ai.ai.prompts import merge_prompt_with_articles
 from hackerman_ai.ai.utils import get_agent_instance_from_model_name
 from hackerman_ai.article.models import ArticlesList, SelectedArticle, SelectedArticlesList
 from hackerman_ai.sources.the_hacker_news import TheHackerNewsSource
@@ -13,8 +12,15 @@ def _get_articles() -> ArticlesList:
     return TheHackerNewsSource().get_articles()
 
 
+def _merge_prompt_with_articles(prompt: str, articles: ArticlesList) -> str:
+    return f"""{prompt}
+                This is the json:
+                {articles}
+            """
+
+
 def _classify_articles(articles: ArticlesList, prompt: str, agent: BaseAgent, iterations: int) -> SelectedArticlesList:
-    full_prompt = merge_prompt_with_articles(prompt, articles)
+    full_prompt = _merge_prompt_with_articles(prompt, articles)
     return agent.get_prompt_result(prompt=full_prompt, iterations=iterations)
 
 
