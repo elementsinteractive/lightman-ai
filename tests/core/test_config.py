0fadf597ef54929bac74ead99b79afcac7475b26
sdn4z <13658011+sdn4z@users.noreply.github.com>
2025-06-25 12:39:15 +0200

    feat: load prompts from prompt file

--- tests/core/test_config.py
+++ tests/core/test_config.py
@@ -1,8 +1,8 @@
-from unittest.mock import Mock, call, patch
+import pathlib
 
 import pytest
-from hackerman_ai.constants import DEFAULT_CONFIG_SECTION
-from hackerman_ai.core.config import CONFIG_FILE, FileConfig, FinalConfig, PromptConfig, get_fpath
+from hackerman_ai.constants import DEFAULT_CONFIG_FILE, DEFAULT_CONFIG_SECTION
+from hackerman_ai.core.config import FileConfig, FinalConfig, PromptConfig
 from hackerman_ai.core.exceptions import ConfigNotFoundError, InvalidConfigError, PromptNotFoundError
 from pydantic import ValidationError
 from tests.conftest import patch_config_file
@@ -18,7 +18,7 @@ class TestConfig:
         prompt = 'eval-prompt'
         """
         with patch_config_file(content=content):
-            config = FileConfig.get_config_from_file(config_section=DEFAULT_CONFIG_SECTION)
+            config = FileConfig.get_config_from_file(config_section=DEFAULT_CONFIG_SECTION, path=DEFAULT_CONFIG_FILE)
 
         assert config.iterations == 3
         assert config.model == "gpt-4.1"
@@ -28,7 +28,7 @@ class TestConfig:
     def test_get_from_file_empty(self) -> None:
         content = ""
         with patch_config_file(content=content):
-            config = FileConfig.get_config_from_file(config_section=DEFAULT_CONFIG_SECTION)
+            config = FileConfig.get_config_from_file(config_section=DEFAULT_CONFIG_SECTION, path=DEFAULT_CONFIG_FILE)
 
         assert config.iterations is None
         assert config.model is None
@@ -38,7 +38,7 @@ class TestConfig:
         content = """[default]
         random_key = 1"""
         with pytest.raises(ValidationError), patch_config_file(content):
-            FileConfig.get_config_from_file(config_section=DEFAULT_CONFIG_SECTION)
+            FileConfig.get_config_from_file(config_section=DEFAULT_CONFIG_SECTION, path=DEFAULT_CONFIG_FILE)
 
     def test_load_different_config(self) -> None:
         content = """[default]
@@ -46,49 +46,31 @@ class TestConfig:
         [settings]
         iterations = 2"""
         with patch_config_file(content):
-            config = FileConfig.get_config_from_file(config_section="settings")
+            config = FileConfig.get_config_from_file(config_section="settings", path=DEFAULT_CONFIG_FILE)
         assert config.iterations == 2
-
-    def test_get_from_specific_path_found(self) -> None:
-        with patch_config_file(exists=False), pytest.raises(ConfigNotFoundError):
-            FileConfig.get_config_from_file(config_section=DEFAULT_CONFIG_SECTION, path="fake_path")
 
     def test_get_from_file_config_not_found_and_no_path_specified(self) -> None:
         with patch_config_file(exists=False):
-            config = FileConfig.get_config_from_file(
-                config_section=DEFAULT_CONFIG_SECTION,
-            )
+            config = FileConfig.get_config_from_file(config_section=DEFAULT_CONFIG_SECTION, path=DEFAULT_CONFIG_FILE)
 
         assert config.iterations is None
         assert config.model is None
         assert config.score_threshold is None
         assert config.prompt is None
 
-    @pytest.mark.parametrize(
-        ("path", "expected"),
-        [
-            ("my_path/my_file", "my_path/my_file"),
-            ("", CONFIG_FILE),
-        ],
-    )
-    def test_get_fpath(self, path: str, expected: str) -> None:
-        assert str(get_fpath(path=path)) == expected
-
-    @patch("hackerman_ai.core.config.get_fpath", wraps=get_fpath)
-    def test_file_is_loaded_from_location(self, m_fpath: Mock) -> None:
-        with patch_config_file(
-            content="""
+    def test_file_is_loaded_from_location(self, tmp_path: pathlib.Path) -> None:
+        path = "my_path.toml"
+        content = """
         [default]
         iterations = 3
         model = 'gpt-4.1'
         score_threshold = 8
         prompt = 'eval-prompt'
         """
-        ):
-            config = FileConfig.get_config_from_file(config_section=DEFAULT_CONFIG_SECTION, path="my_path")
+        fpath = tmp_path / path
+        fpath.write_text(content)
+        config = FileConfig.get_config_from_file(config_section=DEFAULT_CONFIG_SECTION, path=str(fpath))
 
-        assert m_fpath.call_count == 1
-        assert m_fpath.call_args == call("my_path")
         assert config.iterations == 3
         assert config.model == "gpt-4.1"
         assert config.score_threshold == 8
@@ -130,3 +112,19 @@ class TestPromptConfig:
         with pytest.raises(PromptNotFoundError) as exc:
             PromptConfig(prompts={}).get_prompt("aaa")
         assert exc.value.args[0] == "prompt `aaa` not found in config file"
+
+    def test_prompt_is_loaded_from_file(self, tmp_path: pathlib.Path) -> None:
+        path = "my_path.toml"
+        content = """
+        [prompts]
+        eval = "my eval prompt"
+        """
+        fpath = tmp_path / path
+        fpath.write_text(content)
+        config = PromptConfig.get_config_from_file(path=str(fpath))
+
+        assert config.prompts == {"eval": "my eval prompt"}
+
+    def test_raises_error_if_file_not_found(self) -> None:
+        with patch_config_file(exists=False), pytest.raises(ConfigNotFoundError):
+            PromptConfig.get_config_from_file(path="fake_path")
